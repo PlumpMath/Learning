d604b60dd26a0c9e7dcc961e2bafb4831076e1d6
Esperanza Moreno Cruz <hola@mi-mina.com>
2015-09-10 12:17:55 +0200

    cambio

---
+++ lighttableusage.clj
@@ -0,0 +1,9 @@
+;; Shortcuts
+
+;; Ctrl+Enter Evalúa esa línea
+;; Ctrl+Shift+Enter Evalúa todo
+;; Ctrl+O Abre el Navigate pane
+;; Ctrl+Space Abre el Command pane
+
+;; Ctrl+t autoformat. Añadido por mi en user.keymap
+;; Ctrl+shift+w Abre el Workspace pane. Añadido por mi en user.keymap
